08387594eaaa635b14254468c49fe6baec664e5f
dlee_debian <darrenlee821@gmail.com>
2025-02-25 05:02:49 +0800

    resolve ldd naming conflict in CI environment

--- base_external/package/ldd3/ldd.mk
+++ base_external/package/ldd3/ldd.mk
@@ -1,14 +1,14 @@
-LDD_VERSION = d9c8eeb916f4f39658888e92d6c8e05cd437882e
-LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-DL821at.git
-LDD_SITE_METHOD = git
-LDD_GIT_SUBMODULES = YES
-LDD_MODULE_SUBDIRS += scull
-LDD_MODULE_SUBDIRS += misc-modules
+LDD3_VERSION = d9c8eeb916f4f39658888e92d6c8e05cd437882e
+LDD3_SITE = git@github.com:cu-ecen-aeld/assignment-7-DL821at.git
+LDD3_SITE_METHOD = git
+LDD3_GIT_SUBMODULES = YES
+LDD3_MODULE_SUBDIRS += scull
+LDD3_MODULE_SUBDIRS += misc-modules
 
 # Explicitly define the kernel version used by Buildroot.
 KERNEL_VER = 6.6.50
 
-define LDD_INSTALL_TARGET_CMDS
+define LDD3_INSTALL_TARGET_CMDS
 	# Create the target directory for external modules.
 	mkdir -p $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/updates
 	$(INSTALL) -D -m 0644 $(BUILD_DIR)/scull/scull.ko $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/updates/scull.ko
